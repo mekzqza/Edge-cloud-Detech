489a8f391ceeaa928da728925b12c40d86bb48e6
mekzqza <facebook007me@gmail.com>
2026-07-29 17:07:57 +0700

    add internalSecret

--- backend/src/routes/oauth.js
+++ backend/src/routes/oauth.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
 const router = Router();
+const INTERNAL_SECRET = env.INTERNAL_SECRET; //
 
-router.get("/oauth", async (req, res) => {
-  res.json({ message: "OAuth route is working!" });
+router.post("/oauth", async (req, res) => {
+  const internalSecret = req.headers["x-internal-secret"];
+  if (internalSecret === null || internalSecret !== INTERNAL_SECRET) {
+    return res.status(403).json({ error: "Forbidden" });
+  }
+  res.json({ ok: true, got: req.body });
 });
 
 module.exports = router;
